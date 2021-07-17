c86e5e797b4d8e10374af012b3b2783a02e2b2c9
13jun <820373584@qq.com>
2021-07-17 13:37:36 +0800

    实现懒加载

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import x from './test.js'
 import './x.css'
 import myimage from './test.png'
+
 console.log('123')
 console.log(x)
 
@@ -12,3 +13,16 @@ function component(){
     return element
 }
 document.body.appendChild(component())
+
+const button = document.createElement('button')
+button.innerText='懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy.js')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{
+        console.log('加载错误')
+    })
+}
+document.body.appendChild(button)
