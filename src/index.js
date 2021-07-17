47e634aef7c1b9e7fa6c2292e18b7543e06c86bc
13jun <820373584@qq.com>
2021-07-17 12:52:58 +0800

    css引入&&图片引入

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import x from './test.js'
+import './x.css'
+import myimage from './test.png'
 console.log('123')
 console.log(x)
+
+function component(){
+    const element = document.createElement('div')
+    const image = new Image()
+    image.src = myimage;
+    element.appendChild(image)
+    return element
+}
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode:'development',
     entry:'./src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+      },
     output:{
         filename:'index.[contenthash].js',
         path:path.resolve(__dirname,'dist')
@@ -15,5 +21,17 @@ module.exports = {
         filename: 'index.html',
         template: './index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
 }
